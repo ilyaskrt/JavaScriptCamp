e2dfbfc1e8e872ae58f0035ad797647cc18ccef8
İlyas <ilyaskrt19@gmail.com>
2021-10-18 23:30:37 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# JavaScriptCamp
+# advancedJavaScript
+Starter kit for my JS Courses
---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("merhaba Dünya")
+
+//js tip güvenli değil 
+
+let dolarDun = 9.20
+
+let konutKredileri =["Konut","Emlak","Kamu"]
+
+console.log(konutKredileri)
