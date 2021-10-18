d0d17bb6813391a4a1e570579b29728eafa33178
İlyas <ilyaskrt19@gmail.com>
2021-10-18 23:57:14 +0300

    First Night End

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
-console.log("merhaba Dünya")
-
-//js tip güvenli değil 
-
+//js tip güvenli değil başta ne tanımlarsan öyle devam eder
 let dolarDun = 9.20
+let dolarBugun = 9.30
 
-let konutKredileri =["Konut","Emlak","Kamu"]
+console.log(dolarDun)
 
+// const ile atanan veri sabit başka bir şey atayamazsın 
+const euroDun =11.2
+
+console.log(euroDun)
+let konutKredileri =["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+    for(let i = 0 ; i<konutKredileri.length; i++){
+       console.log("<li>"+ konutKredileri[i] +"</li>")
+    }
+console.log("</ul>")
+
+
+ //{
+//     <ul>
+//         <li>Konut</li>
+//         <li>Emlak</li>
+//         <li>Kamu</li>
+//     </ul>
+
+// }
 console.log(konutKredileri)
